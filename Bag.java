be0a099cb0488e1567c12a24a8711024ee694c54
rv-gowda <prathamkrgowda@gmail.com>
2026-02-04 00:13:37 +0530

    Add files via upload

---
+++ Bag.java
@@ -0,0 +1,32 @@
+public class Bag{
+	
+	static void books() { 
+	
+	System.out.println("Bag contains books.");
+	
+	} 
+	
+	 static void laptop() {
+	
+		System.out.println("Bag contains a laptop.");
+	
+    }
+		
+	static void charger() { 
+	   
+	   System.out.println("Bag contains a charger."); 
+	
+	}
+
+	static void waterBottle() { 
+	
+		System.out.println("Bag contains a water bottle."); 
+	
+	}
+	
+	static void snacks() { 
+		
+		System.out.println("Bag contains snacks."); 
+	
+	} 
+}
