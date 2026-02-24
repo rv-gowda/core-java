010e2ee505c537ef7a6f4200d692d3b91314492c
rv-gowda <prathamkrgowda@gmail.com>
2026-02-24 11:12:58 +0530

    Add files via upload

--- Mobile.java
+++ Mobile.java
@@ -1,49 +1,11 @@
-public class Mobile{
-  
-	static void call() { 
-		System.out.println("Calling feature activated."); 
-	} 
-	static void message() { 
-	
-		System.out.println("Messaging feature activated."); 
-	} 
-	
-	static void camera() { 
-	
-		System.out.println("Camera feature activated."); 
-	} 
-	
-	static void music() {
-		
-		System.out.println("Music player feature activated."); 
-	} 
-	
-	static void video() { 
-	
-		System.out.println("Video player feature activated."); 
-	} 
-	
-	static void bluetooth() { 
-	
-		System.out.println("Bluetooth feature activated."); 
-	} 
-	
-	static void wifi() {
-		
-		System.out.println("Wi-Fi feature activated."); 
-	} 
-	
-	static void gps() { 
-		
-		System.out.println("GPS feature activated."); 
-	}
+public class Mobile {
 
-	static void games() { 
+    static void register(String patientName, String symptom, String email, long mobile) {
+        System.out.println("Mobile App Registration Successful!");
+        System.out.println("Patient: " + patientName);
+		System.out.println("------------------------------------");
+        Reception.book(patientName, symptom, email, mobile);
+		System.out.println("------------------------------------");
 		
-		System.out.println("Games feature activated."); 
-	} 
-	static void browser() { 
-	
-		System.out.println("Browser feature activated.");
-	}
+    }
 }
---
+++ Nurse.java
@@ -0,0 +1,13 @@
+public class Nurse {
+
+    static void basicCheck(String patientName, String symptom, int bp, boolean sugar) {
+        System.out.println("Nurse checking patient: " + patientName);
+        System.out.println("Symptom: " + symptom);
+        System.out.println("BP: " + bp);
+        System.out.println("Sugar: " + (sugar ? "High" : "Normal"));
+		System.out.println("------------------------------------");
+        Doctor.treat(Name, Symptoms);
+		System.out.println("------------------------------------");
+
+    }
+}
